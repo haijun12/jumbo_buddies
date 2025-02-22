bf2ad7f8a39cbf5f6c99f9bf4b17014cbe6c1d9c
Haijun Si <sihaijun1@gmail.com>
2025-02-22 15:39:36 -0500

    database table

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -7,8 +7,49 @@ if (!databaseUrl) {
 }
 export const sql = neon(process.env.DATABASE_URL as string);
 
+// TEST FUNCTION
 export async function getData() {
     const response = await sql`SELECT version()`;
     console.log(response);
     return response[0].version;
+}
+
+async function ensureTables() {
+    try {
+        const createUserSchema = sql`CREATE SCHEMA IF NOT EXISTS Users`;
+        const createUserTable = sql`
+            CREATE TABLE IF NOT EXISTS Users.User (
+            id SERIAL PRIMARY KEY
+        )
+        `
+        const createUserListTable = sql`
+            CREATE TABLE IF NOT EXISTS Users.Lists (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                create_date DATE NOT NULL,
+                update_date DATE NOT NULL,
+                        user_id INTEGER REFERENCES Users.User(id) ON DELETE CASCADE NOT NULL
+            )
+        `
+        const createUserRatingItemTable = sql`
+            CREATE TABLE IF NOT EXISTS Users.rating_item (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                description TEXT,
+                image TEXT, -- a link to the image db
+                rank INTEGER NOT NULL,
+                type ENUM('Bad', 'Ok', 'Good') NOT NULL,
+                list INTEGER REFERENCES Users.Lists(id) ON DELETE CASCADE NOT NULL
+            )
+        `;
+        await Promise.all([
+            createUserSchema,
+            createUserTable,
+            createUserListTable,
+            createUserRatingItemTable,
+        ]);
+    } catch (error) {
+        console.error("Error creating visit database:", error);
+        throw error;
+    }
 }
