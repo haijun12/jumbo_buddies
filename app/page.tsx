f8536c99ab375588fea94d0763c77e76ab8f6c82
Charles Mitchell <charlesmitchell@MacBook-Pro-14.local>
2025-02-22 16:30:15 -0500

    Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import Home from "@/features/home";
-
-export default function Page() {
+export default function Home() {
   return (
-    <Home></Home>
+    <div
+      className="min-h-screen flex flex-col justify-center items-center text-center bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/pictures/homeBackground.png')" }}
+    >
+      <h1 className="text-8xl font-alice-bold text-white mb-4">
+        DOWN THE RABBIT HOLE
+      </h1>
+      <p className="text-3xl text-white mb-6">
+        Rate Everything & Anything that You've Ever Done, Eaten, Seen, Read, etc.
+      </p>
+      <button className="bg-black text-white px-6 py-3 rounded">
+        START
+      </button>
+    </div>
   );
 }
