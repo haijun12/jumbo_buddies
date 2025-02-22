fdff790d13ae210c46b2b6ea93368680e563d90b
Charles Mitchell <charlesmitchell@MacBook-Pro-14.local>
2025-02-22 17:48:47 -0500

    new dependencies

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,33 +1,37 @@
 "use client";
 import { useState } from "react";
-import EventVotingCard from "@/components/EventVotingCard";
+import EventVotingCard from "../components/EventVotingCard";
+
 export default function Lists() {
-    const [showCard, setShowCard] = useState(false);
-    return (
-        <button
+  const [showCard, setShowCard] = useState(false);
+
+  return (
+    <>
+      <button
         className="bg-black text-white px-6 py-3 rounded"
         onClick={() => setShowCard(true)}
       >
         START
       </button>
-    
-    {showCard && (
+
+      {showCard && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           {/* The Event Card in the center */}
           <EventVotingCard onClose={() => setShowCard(false)} />
 
           {/* The two images on the bottom-left and bottom-right */}
           <img
-            src="/pictures/Tweedledee 2.png" 
+            src="/pictures/Tweedledee 2.png"
             alt="Girl on the left"
             className="absolute bottom-0 left-0 w-32 h-auto"
           />
           <img
-            src="/pictures/Tweedledee 1.png" 
+            src="/pictures/Tweedledee 1.png"
             alt="Girl on the right"
             className="absolute bottom-0 right-0 w-32 h-auto"
           />
         </div>
       )}
-    )
+    </>
+  );
 }
