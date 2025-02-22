29721a938351105050eb8a1d3e9f13f81a3b7b6a
Haijun Si <sihaijun1@gmail.com>
2025-02-22 17:47:39 -0500

    session id with sign out?

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,18 @@
-import { SignOutButton } from '@clerk/nextjs'
+'use client'
+
+import { SignInButton, SignOutButton, useAuth } from '@clerk/nextjs'
+
 export default function Profile() {
+    const { sessionId } = useAuth()
+
+    if (!sessionId) {
+        return <SignInButton />
+    }
+
     return (
         <div className="flex flex-col"> 
         show the profile
-        <SignOutButton>
+        <SignOutButton signOutOptions={{ sessionId }}>
             <button className="bg-black text-white px-6 py-3 rounded">
                 Sign Out
             </button>
