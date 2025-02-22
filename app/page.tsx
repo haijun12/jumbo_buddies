089b6624f8c5a3a80282b383aef249094720c07c
Charles Mitchell <charlesmitchell@MacBook-Pro-14.local>
2025-02-22 17:13:53 -0500

    moving things to lists

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
-import { useState } from "react";
-import EventVotingCard from "./components/EventVotingCard";
+
 
 import Link from "next/link"
 export default function Home() {
-  const [showCard, setShowCard] = useState(false);
+
 
   return (
     <div
@@ -19,29 +17,11 @@ export default function Home() {
       </p>
       <Link href="/create"
         className="bg-black text-white px-6 py-3 rounded"
-        onClick={() => setShowCard(true)}
       >
         START
       </Link>
 
-      {showCard && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          {/* The Event Card in the center */}
-          <EventVotingCard onClose={() => setShowCard(false)} />
-
-          {/* The two images on the bottom-left and bottom-right */}
-          <img
-            src="/pictures/Tweedledee 2.png" 
-            alt="Girl on the left"
-            className="absolute bottom-0 left-0 w-32 h-auto"
-          />
-          <img
-            src="/pictures/Tweedledee 1.png" 
-            alt="Girl on the right"
-            className="absolute bottom-0 right-0 w-32 h-auto"
-          />
-        </div>
-      )}
+      
     </div>
   );
 }
