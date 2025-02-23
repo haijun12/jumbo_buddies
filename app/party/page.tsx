95ae96647bd3d03df533f1e82cf396fc1533e565
phuongthanh-nguyen <phuongnguyen202012@gmail.com>
2025-02-22 20:08:26 -0500

    party page created

---
+++ app/party/page.tsx
@@ -0,0 +1,22 @@
+export default function Party() {
+    return (
+      <div className="min-h-screen flex flex-col">
+        {/* Title at the top */}
+        <div className="p-4 text-center">
+          <h1 className="font-alice text-[75px] font-bold mb-4">
+            Whimsical Tea Party
+          </h1>
+        </div>
+  
+        {/* Body with a set height, centered content */}
+        <div className="h-[600px] flex items-center justify-center p-4 text-center">
+          <p className="text-[32px] italic text-[#676767]">
+            Invite friends to join the tea party 🫖🍰
+            <br />
+            (Feature is currently unavailable)
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
