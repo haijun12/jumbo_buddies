5bab50582284c8be3629560c00a64892ed7ee778
Charles Mitchell <charlesmitchell@MacBook-Pro-14.local>
2025-02-23 14:45:20 -0500

    new home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,55 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div
-      className="min-h-screen flex flex-col justify-center items-center text-center bg-cover bg-center bg-no-repeat w-full"
-      style={{ backgroundImage: "url('/pictures/homeBackground.png')" }}
+      className="flex items-center justify-center w-full min-h-screen"
+      style={{ backgroundColor: "#7B1F1F" }} // Maroon background
     >
-      <h1 className="text-8xl font-alice font-bold text-white mb-[50px]">
-        DOWN THE RABBIT HOLE
-      </h1>
-      <p className="text-3xl text-white italic mb-[50px]">
-        Rate Everything & Anything that You&apos;ve Ever Done, Eaten, Seen, Read, etc.
-      </p>
-      <Link href="/collections" className="my-[20px] p-[20px] bg-black text-white">
-        START
-      </Link>
+      {/* Scroll container (relative) */}
+      <div className="relative">
+        {/* The scroll image */}
+        <img
+          src="/pictures/scroll 1.png"
+          alt="Scroll"
+          className="block w-auto max-w-full"
+        />
+
+        {/* Heading (absolute, near the top) */}
+        <div className="absolute top-[20%] w-full text-center left-1/2 -translate-x-1/2">
+          <h1 className="text-6xl font-alice text-black mb-[50px]">DOWN THE</h1>
+
+        </div>
+
+        <div className="absolute top-[22%] left-[70%] w-full text-center left-1/2 -translate-x-1/2">
+          <img
+            src="/pictures/Rabbit.png"
+            alt="Rabbit"
+            className="block w-auto max-w-full"
+          />
+        </div>
+
+        <div className="absolute top-[35%] left-[96%] w-full text-center left-1/2 -translate-x-1/2">
+          <img
+            src="/pictures/Hole.png"
+            alt="Hole"
+            className="block w-auto max-w-full"
+          />
+        </div>
+
+        {/* Subheading (absolute, a bit lower) */}
+        <div className="absolute top-[57%] w-full text-center left-1/2 -translate-x-1/2 px-4">
+          <p className="text-lg italic">
+            Are you ready to rate everything & anything that <br/>you've ever done, 
+            eaten, seen, read, and more?
+          </p>
+        </div>
+
+        {/* START Button (absolute, near bottom to align with wax seal) */}
+        <div className="absolute bottom-[25%] left-[40%] w-full text-center left-1/2 -translate-x-1/2">
+          <Link href="/collections" className="bg-black text-white px-11 py-7 rounded text-2xl">
+            START
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
