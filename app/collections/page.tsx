0e90b7ff61a42919f5d905c15e67f1d50794c69d
Haijun Si <sihaijun1@gmail.com>
2025-02-22 20:13:27 -0500

    button fixed

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { useState } from "react";
 import TextField from '@mui/material/TextField';
-import { Play, X } from 'lucide-react';
 import { createRatingList } from '@/app/lib/api';
 import { useRouter } from "next/navigation";
 
 
 export default function Create() {
   const router = useRouter();
-  const [isClicked, setIsClicked] = useState(false);
   const [name, setName] = useState("Name the list");
 
   const handleFocus = () => {
@@ -24,22 +22,12 @@ export default function Create() {
 
   return (
     <div className="fixed bottom-[30px] left-1/2 transform -translate-x-1/2">
-      {!isClicked ?(
-      <button 
-            className={`text-white font-bold my-4 p-4 ${isClicked ? "bg-gray-500" : "bg-black"}`} 
-            onClick={() => setIsClicked(!isClicked)} 
-        >
-            CREATE LIST
-        </button>
-      ) : (
       <div className="flex flex-row items-center gap-4">
-        <button onClick={()=> setIsClicked(!isClicked)} className="h-[60px] my-[20px] p-[20px] bg-black text-white">
-          <X className="fill-white" />
-        </button>
         <TextField
           placeholder="Name the list"
           value={name} 
           onChange={(e) => setName(e.target.value)}
+          onFocus={handleFocus}
           sx={{
             '& .MuiOutlinedInput-root': {
               borderRadius: 0,
@@ -50,11 +38,10 @@ export default function Create() {
           }}
         />
 
-        <button onClick={handleOnNext} className="h-[60px] my-[20px] p-[20px] bg-black text-white">
-          <Play className="fill-white" />
+        <button onClick = {handleOnNext} className="h-[60px] my-[20px] p-[20px] bg-black text-white">
+          CREATE
         </button>
       </div>
-      )}
     </div>
 
   );
