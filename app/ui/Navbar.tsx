08798c77650ab5325bfea3bb9dfa80b23d62ad5a
Haijun Si <sihaijun1@gmail.com>
2025-02-22 19:06:28 -0500

    updated nav bar

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,39 +1,44 @@
 "use client";
 import { usePathname } from "next/navigation";
 import styles from './nav.module.css';
+import Link from "next/link";
+
 export default function Navbar() {
     const pathname = usePathname(); // Get the current route
 
     // Define public routes where the Navbar should not be displayed
     const isPublicPage = ["/", "/sign-in", "/sign-up"].includes(pathname);
+    if (isPublicPage) return null;
+    console.log(pathname);
+
     return (
-        
+     
       <nav className={styles.navbar}>
       <div className={styles["navbar-container"]}>
         <ul className={styles["navbar-list"]}>
           <li>
-            <a href="/" className={styles["navbar-item"]}>
+            <Link href="/" className={styles["navbar-item"]}>
               <img src="/pictures/white-rabbit.png" alt="White Rabbit" />
-              <span>BACK</span>
-            </a>
+              <span className={`${pathname === "/" ? "bg-yellow-400 text-white " : ""} rounded-full p-2 px-3`}>HOME</span>
+            </Link>
           </li>
           <li>
-            <a href="/collection" className={styles["navbar-item"]}>
+            <Link href="/create" className={styles["navbar-item"]}>
               <img src="/pictures/mad-hatter.png" alt="Mad Hatter" />
-              <span>COLLECTION</span>
-            </a>
+              <span className={`${pathname === "/create" ? "bg-yellow-400 text-white " : ""} rounded-full p-2 px-3`}>COLLECTION</span>
+            </Link>
           </li>
           <li>
-            <a href="/party" className={styles["navbar-item"]}>
+            <Link href="/party" className={styles["navbar-item"]}>
               <img src="/pictures/teapot.png" alt="Teapot" />
-              <span>PARTY</span>
-            </a>
+              <span className={`${pathname === "/party" ? "bg-yellow-400 text-white " : ""} rounded-full p-2 px-3`}>PARTY</span>
+            </Link>
           </li>
           <li>
-            <a href="/mirror" className={styles["navbar-item"]}>
+            <Link href="/profile" className={styles["navbar-item"]}>
               <img src="/pictures/mirror.png" alt="Mirror" />
-              <span>PROFILE</span>
-            </a>
+              <span className={`${pathname === "/profile" ? "bg-yellow-400 text-white " : ""} rounded-full p-2 px-3`}>PROFILE</span>
+            </Link>
           </li>
         </ul>
       </div>
@@ -41,5 +46,6 @@ export default function Navbar() {
     );
   }
   
+
   
   
